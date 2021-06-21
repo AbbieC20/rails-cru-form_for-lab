012eed6f3ec5c6e0ae08cfe94bd27f27c86fee9b
Abbie Cornelius <abbyc@sky.com>
2021-06-21 20:26:00 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :artists, only: [:show, :new, :create, :edit, :update]
+  patch 'artists/:id', to: 'artists#update'
+  resources :genres, only: [:show, :new, :create, :edit, :update]
+  patch 'genres/:id', to: 'genre#update'
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
+  patch 'songs/:id', to: 'song#update'
 end
